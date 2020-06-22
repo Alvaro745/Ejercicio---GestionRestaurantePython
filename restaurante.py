98424b5e4f58a40450281cc350edb90593bbf644
Alvaro745 <alvaro.v745@gmail.com>
2020-06-22 07:16:06 -0400

    Se creo una nueva funcion para buscar por tipos de platos.

--- restaurante.py
+++ restaurante.py
@@ -17,7 +17,7 @@ class Restaurantes:
         5.- DAR DE ALTA AL PLATO
         6.- DAR DE BAJA AL PLATO
         7.- MOSTRAR PLATOS DE BAJA
-        8.- TIPOS DE PLATOS CLASIFICADOS
+        8.- BUSCAR POR TIPO DE PLATO
         9.- Salir
         """
 
@@ -60,14 +60,14 @@ class Restaurantes:
         return "-------Transacciones Terminadas-----------"
 
     def agregarPlato(self):
-        codigo = input("Codigo de Plato: \n")
-        nombre = input("Nombre del Plato: \n")
-        precio = int(input("Precio del Plato: \n"))
-        descuento = int(input("Digite el porcentaje del Descuento para el Plato: \n"))
-        descripcion = input("Descripcion del Plato: \n")
-        tipo = input("Tipo de Plato: \n")
+        codigo = input("Codigo de Plato de comida: \n")
+        nombre = input("Nombre del Plato de comida: \n")
+        precio = int(input("Precio del Plato de comida: \n"))
+        descuento = int(input("Digite el porcentaje del Descuento para el Plato de comida: \n"))
+        descripcion = input("Descripcion del Plato de comida: \n")
+        tipo = input("Tipo de Plato de comida: \n")
         print(self.guardarPlato(codigo, nombre, precio, descuento, descripcion, tipo))
-        agregarOtro = input("Desea agregar otro Plato? y/n \n")
+        agregarOtro = input("Desea agregar otro Plato de comida? y/n \n")
         if (agregarOtro == 'y' or agregarOtro == 'Y'):
             self.agregarPlato()
 
@@ -79,7 +79,7 @@ class Restaurantes:
         self.descripcion.append(descripcion)
         self.tipo.append(tipo)
         self.habilitado.append('si')
-        return "El Plato {} fue agregado correctamente".format(nombre)
+        return "El Plato de comida {} fue agregado correctamente".format(nombre)
 
     def verPlatoAlta(self):
         return self.inventarioPlato('si')
@@ -93,7 +93,7 @@ class Restaurantes:
                 self.descripcionesPlato(i, habilitado)
             return "Inventario Cargado Correctamente"
         else:
-            return "TODAVIA NO SE AGREGARON PRODUCTOS A LA BASE DE DATOS"
+            return "TODAVIA NO SE AGREGARON PLATOS DE COMIDAS A LA BASE DE DATOS"
     
     def MenuTodoPlato(self):
         print(self.verPlatoAlta())
@@ -101,74 +101,63 @@ class Restaurantes:
 
     def descripcionesPlato(self, posicion, habilitado):
         if(self.habilitado[posicion] == habilitado):
-            print("*****DESCRIPCION DEL PLATO {}*****".format(self.nombre[posicion]))
-            print("CODIGO DEL PLATO: {}".format(self.codigo[posicion]))
-            print("PRECIO DEL PLATO: {} Bs".format(self.precio[posicion]))
+            print("*****DESCRIPCION DEL PLATO DE COMIDA {}*****".format(self.nombre[posicion]))
+            print("CODIGO DEL PLATO DE COMIDA: {}".format(self.codigo[posicion]))
+            print("PRECIO DEL PLATO DE COMIDA: {} Bs".format(self.precio[posicion]))
             print("PRECIO CON DESCUENTO: {} Bs".format(self.descuento[posicion]))
-            print("DESCRIPCION DEL PLATO: {}".format(self.descripcion[posicion]))
-            print("TIPO DEL PLATO: {}".format(self.tipo[posicion]))
+            print("DESCRIPCION DEL PLATO DE COMIDA: {}".format(self.descripcion[posicion]))
+            print("TIPO DEL PLATO DE COMIDA: {}".format(self.tipo[posicion]))
             print("HABILITADO: {}".format(self.habilitado[posicion]))
             print("**********************************************")
             pass
 
-    def descripcionesPlatobuscador(self, posicion):
-        print("*****DESCRIPCION DEL PLATO {}*****".format(self.nombre[posicion]))
-        print("CODIGO DEL PLATO: {}".format(self.codigo[posicion]))
-        print("PRECIO DEL PLATO: {} Bs".format(self.precio[posicion]))
-        print("PRECIO CON DESCUENTO: {} Bs".format(self.descuento[posicion]))
-        print("DESCRIPCION DEL PLATO: {}".format(self.descripcion[posicion]))
-        print("TIPO DEL PLATO: {}".format(self.tipo[posicion]))
-        print("HABILITADO: {}".format(self.habilitado[posicion]))
-        print("**********************************************")
-        pass
-
     def editarPlato(self):
-        print("*********ACTUALIZAR EL PLATO**********")
+        print("*********ACTUALIZAR EL PLATO DE COMIDA**********")
         posicion = self.buscarPlato(1)
         # self.descripcionesPlato()
-        nuevo_precio = int(input("Digite el nuevo precio del plato {}: \n".format(self.nombre[posicion])))
-        nuevo_descuento = int(input("Digite el nuevo porcentaje de descuento para el plato {}: \n".format(self.nombre[posicion])))
-        nuevo_descripcion = input("Digite la nueva descripcion para el plato {}: \n".format(self.nombre[posicion]))
-        nuevo_tipo = input("Digite el nuevo tipo de plato entre (Desayuno, Almuerzo, Cena y Postre) para el nuevo Plato: \n")
+        nuevo_precio = int(input("Digite el nuevo precio del plato de comida {}: \n".format(self.nombre[posicion])))
+        nuevo_descuento = int(input("Digite el nuevo porcentaje de descuento para el plato de comida {}: \n".format(self.nombre[posicion])))
+        nuevo_descripcion = input("Digite la nueva descripcion para el plato de comida {}: \n".format(self.nombre[posicion]))
+        nuevo_tipo = input("Digite el nuevo tipo de plato de comida entre (Desayuno, Almuerzo, Cena y Postre) para el nuevo Plato: \n")
         self.precio[posicion] = nuevo_precio
         self.descuento[posicion] = nuevo_descuento
         self.descripcion[posicion] = nuevo_descripcion
         self.tipo[posicion] = nuevo_tipo
         print(self.modificarPlato(posicion, nuevo_precio, nuevo_descuento, nuevo_descripcion, nuevo_tipo))
         # self.descripcionesPlato(posicion)
-        return "Modificacion del Plato en el Menu Completado"
+        return "Modificacion del Plato de comida en el Menu Completado"
 
     def modificarPlato(self, posicion, precio, descuento, descripcion, tipo):
         self.precio[posicion] = precio
         self.descuento[posicion] = precio - (precio * descuento / 100)
         self.descripcion[posicion] = descripcion
         self.tipo[posicion] = tipo
-        return "El Plato {} del Menu fue Modificado Correctamente".format(self.nombre[posicion])
+        return "El Plato de comida {} del Menu fue Modificado Correctamente".format(self.nombre[posicion])
 
     def buscarPlato(self, habilitado):
         print(self.inventarioPlato(habilitado))
-        eleccion = input("Digite el Codigo del Plato: \n")
+        eleccion = input("Digite el Codigo del Plato de comida: \n")
         posicion = self.codigo.index(eleccion)
         self.inventarioPlato(posicion)
         return posicion
     
     def realizarAlta(self):
-        print("*************DAR DE ALTA UN PLATO*************")
+        print("*************DAR DE ALTA UN PLATO DE COMIDA*************")
         posicion = self.buscarPlato('no')
         return self.darAlta(posicion)
     
     def darAlta(self, posicion):
         self.habilitado[posicion] = 'si'
-        return "El Plato {} esta de Alta..!!".format(self.nombre[posicion])
+        return "El Plato de comida {} esta de Alta..!!".format(self.nombre[posicion])
 
     def realizarBaja(self):
-        print("*************DAR DE BAJA UN PLATO*************")
+        print("*************DAR DE BAJA UN PLATO DE COMIDA*************")
         posicion = self.buscarPlato('si')
         return self.darBaja(posicion)
     
     def darBaja(self, posicion):
         self.habilitado[posicion] = 'no'
-        return "El Plato {} esta de Baja..!!".format(self.nombre[posicion])
+        return "El Plato de comida {} esta de Baja..!!".format(self.nombre[posicion])
 
     def usuarioBuscarTipoPlato(self):
         tipo = input("Escriba el tipo de comida a buscar: \n")
@@ -181,7 +170,18 @@ class Restaurantes:
                 encontrado = True
                 self.descripcionesPlatobuscador(i)
         if encontrado == False:
-            print("No se encontraron resultados con el tipo de almuerzo {}".format(tipo))
+            print("No se encontraron resultados con el tipo {}".format(tipo))
+        pass
+
+    def descripcionesPlatobuscador(self, posicion):
+        print("*****DESCRIPCION DEL PLATO DE COMIDA {}*****".format(self.nombre[posicion]))
+        print("CODIGO DEL PLATO DE COMIDA: {}".format(self.codigo[posicion]))
+        print("PRECIO DEL PLATO DE COMIDA: {} Bs".format(self.precio[posicion]))
+        print("PRECIO CON DESCUENTO: {} Bs".format(self.descuento[posicion]))
+        print("DESCRIPCION DEL PLATO DE COMIDA: {}".format(self.descripcion[posicion]))
+        print("TIPO DEL PLATO DE COMIDA: {}".format(self.tipo[posicion]))
+        print("HABILITADO: {}".format(self.habilitado[posicion]))
+        print("**********************************************")
         pass
 
 restaurante = Restaurantes()
